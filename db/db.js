d62c5ae330ae8bab95c44d6c2506abc46e03a647
Juliana Micheal-Echezona <julianamicheal06@gmail.com>
2023-07-01 00:09:46 +0100

    initial commit

--- db/db.js
+++ db/db.js
@@ -1,54 +1,54 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
+// require('dotenv').config()
+// const mongoose = require('mongoose')
 
-const MONGO_DB_CONNECTION_URL = process.env.MONGO_DB_CONNECTION_URL
+// const MONGO_DB_CONNECTION_URL = process.env.MONGO_DB_CONNECTION_URL
 
-function  connectionMongoDB() {
-    const MONGO_DB_CONNECTION_URL = 'mongodb://localhost:27017'
-    mongoose.connect(MONGO_DB_CONNECTION_URL)};
-//, {erverSelectionTimeoutMS: 30000})
+// function  connectionMongoDB() {
+//     const MONGO_DB_CONNECTION_URL = 'mongodb://localhost:27017'
+//     mongoose.connect(MONGO_DB_CONNECTION_URL)};
+// //, {erverSelectionTimeoutMS: 30000})
 
       
-    mongoose.connection.on('connected', () =>{
-        console.log('connection to mongodb successful')
-    })
-
-    mongoose.connection.on('err', (err) =>{
-        console.error(err)
-        
-    })
-
-
-
- module.exports =   {connectionMongoDB}
-
-
-
-
-
-
-// require('dotenv').config();
-// const mongoose = require('mongoose');
-// //const logger = require("./logger/logger")
-
-// const MONGODB_URI = process.env.MONGODB_URI;
-// //console.log(MONGODB)
-// // connect to mongodb
-// function connectionMongoDB() {
-//     const rl = 'mongodb://localhost:27017'
-//     mongoose.connect(rl, {
-//     serverSelectionTimeoutMS: 30000});
-   
-//     mongoose.connection.on('connected', () => {
-//         console.log('Connection to mongodb successfully')
+//     mongoose.connection.on('connected', () =>{
+//         console.log('connection to mongodb successful')
 //     })
 
-//     mongoose.connection.on('err', (err) => {
+//     mongoose.connection.on('err', (err) =>{
 //         console.error(err)
+        
 //     })
-// }
 
-// module.exports =  {connectionMongoDB}
+
+
+//  module.exports =   {connectionMongoDB}
+
+
+
+
+
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+//const logger = require("./logger/logger")
+
+const MONGODB_URI = process.env.MONGODB_URI;
+//console.log(MONGODB)
+// connect to mongodb
+function connectionMongoDB() {
+    const rl = 'mongodb://localhost:27017'
+    mongoose.connect(rl, {
+    serverSelectionTimeoutMS: 30000});
+   
+    mongoose.connection.on('connected', () => {
+        console.log('Connection to mongodb successfully')
+    })
+
+    mongoose.connection.on('err', (err) => {
+        console.error(err)
+    })
+}
+
+module.exports =  {connectionMongoDB}
 
 
 
